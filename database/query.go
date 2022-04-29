fb4aa529d7ee7bd2539ee74ea10801b4368608c8
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-04-29 22:29:24 +0100

    Modify all the return value of methods and Add RemoveSession and Update methods

--- database/query.go
+++ database/query.go
@@ -1,7 +1,6 @@
 package database
 
 import (
-	"database/sql"
 	"fmt"
 	"forum/password"
 	"time"
@@ -9,109 +8,142 @@ import (
 	uuid "github.com/satori/go.uuid"
 )
 
-var Date = time.Now().Format("2022-January-01")
+var Date = time.Now().Format("2006-January-01 15:04:05")
 
 // start of the create query
 
 // CreateUser
 // is a method of database that add user in it.
-func (forum *Forum) CreateUser(username, email, pass string) (sql.Result, error) {
+func (forum *Forum) CreateUser(username, email, userAgent, ipAddress, pass string) (string, string, error) {
 	userID := uuid.NewV4()
 	pass, _ = password.HashPassword(pass)
 	stmt, _ := forum.DB.Prepare(`
 		INSERT INTO User (userID, username, dateCreated, email, password) values (?, ?, ?, ?, ?)
 	`)
-	r, err := stmt.Exec(userID, username, Date, email, pass)
+	_, err := stmt.Exec(userID, username, Date, email, pass)
 	if err != nil {
-		return r, err
+		return "", "", err
 	}
-	return r, nil
+	sessionID, _ := forum.CreateSession(userID.String(), userAgent, ipAddress)
+	return userID.String(), sessionID, nil
 }
 
 // CreateSession
 // is a method of database that add session in it based on the user login time.
-func (forum *Forum) CreateSession(userID, userAgent, ipAddress string) (sql.Result, error) {
+func (forum *Forum) CreateSession(userID, userAgent, ipAddress string) (string, error) {
 	sessionID := uuid.NewV4()
 	stmt, _ := forum.DB.Prepare(`
-		INSERT INTO User (sessionID, userID, userAgent, ipAddress, Now) values (?, ?, ?, ?, ?)
+		INSERT INTO Session (sessionID, userID, userAgent, ipAddress, loginTime) values (?, ?, ?, ?, ?)
 	`)
-	r, err := stmt.Exec(sessionID, userID, userAgent, ipAddress, Date)
+	// fmt.Print(err1)
+	_, err := stmt.Exec(sessionID, userID, userAgent, ipAddress, Date)
 	if err != nil {
-		return r, err
+		return "", err
 	}
-	return r, nil
+	forum.Update("User", "sessionID", sessionID.String(), "userID", userID)
+	return sessionID.String(), nil
 }
 
 // CreatePost
 // is a method of database that add post in it.
-func (forum *Forum) CreatePost(userID, content, category string) (sql.Result, error) {
+func (forum *Forum) CreatePost(userID, content, category string) (string, error) {
 	postID := uuid.NewV4()
 	stmt, _ := forum.DB.Prepare(`
 		INSERT INTO Post (postID, userID, dateCreated, content, category) values (?, ?, ?, ?, ?)
 	`)
-	r, err := stmt.Exec(postID, userID, Date, content, category)
+	_, err := stmt.Exec(postID, userID, Date, content, category)
 	if err != nil {
-		return r, err
+		return "", err
 	}
-	return r, nil
+	return postID.String(), nil
 }
 
 // CreateComment
 // is a method of database that add comment in it.
-func (forum *Forum) CreateComment(userID, postID, content string) (sql.Result, error) {
+func (forum *Forum) CreateComment(userID, postID, content string) (string, error) {
 	commentID := uuid.NewV4()
 	stmt, _ := forum.DB.Prepare(`
 		INSERT INTO Comment (commentID, userID, postID, dateCreated, content) values (?, ?, ?, ?, ?)
 	`)
-	r, err := stmt.Exec(commentID, userID, postID, Date, content)
+	_, err := stmt.Exec(commentID, userID, postID, Date, content)
 	if err != nil {
-		return r, err
+		return "", err
 	}
-	return r, nil
+	return commentID.String(), nil
 }
 
 // ReactInPost
 // is a method of database that add reaction in the post in it.
-func (forum *Forum) ReactInPost(postID, userID string, react int) (sql.Result, error) {
+func (forum *Forum) ReactInPost(postID, userID string, react int) (string, error) {
 	reactionID := uuid.NewV4()
 	stmt, _ := forum.DB.Prepare(`
 		INSERT INTO Reaction (reactionID, postID, userID, react) values (?, ?, ?, ?)
 	`)
-	r, err := stmt.Exec(reactionID, postID, userID, react)
+	_, err := stmt.Exec(reactionID, postID, userID, react)
 	if err != nil {
-		return r, err
+		return "", err
 	}
-	return r, nil
+	return reactionID.String(), nil
 }
 
 // ReactInComment
 // is a method of database that add reaction in the comment in it.
-func (forum *Forum) ReactInComment(postID, commentID, userID string, react int) (sql.Result, error) {
+func (forum *Forum) ReactInComment(postID, commentID, userID string, react int) (string, error) {
 	reactionID := uuid.NewV4()
 	stmt, _ := forum.DB.Prepare(`
 		INSERT INTO Reaction (reactionID, postID, commentID, userID, react) values (?, ?, ?, ?, ?)
 	`)
-	r, err := stmt.Exec(reactionID, postID, commentID, userID, react)
+	_, err := stmt.Exec(reactionID, postID, commentID, userID, react)
 	if err != nil {
-		return r, err
+		return "", err
 	}
-	return r, nil
+	return reactionID.String(), nil
 }
 
 // start of the delete query
 
 // Delete
 // is a method of the database that delete value base on table and where.
-//  ex. "DELETE FROM table WHERE where = value"
+//  ex. Forum.Delete("User", "userID", "185c6549-caec-4eae-95b0-e16023432ef0")
 func (forum *Forum) Delete(table, where, value string) error {
 	dlt := "DELETE FROM " + table + " WHERE " + where
-	stmt, _ := forum.DB.Prepare(dlt + "= (?)")
-	r, err := stmt.Exec(value)
+	stmt, err1 := forum.DB.Prepare(dlt + " = (?)")
+	fmt.Print(err1)
+	_, err := stmt.Exec(value)
 	if err != nil {
+		fmt.Println("lol")
 		return err
 	}
-	fmt.Print(r)
+	// fmt.Print(stmt)
 	return nil
 }
 
-// Start of the Edit query
+// RemoveSession
+func (forum *Forum) RemoveSession(userID, sessionID string) error {
+	err := forum.Update("User", "sessionID", "", "userID", userID)
+	if err != nil {
+		return err
+	}
+	err = forum.Delete("Session", "sessionID", sessionID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Start of the Update query
+
+// Update
+//  ex. Forum.Update("User", "username", "Adriell", "userID" "7e2b4fdd-86ad-464c-a97e")
+func (forum *Forum) Update(table, set, to, where, id string) error {
+	update := "UPDATE " + table + " SET " + set + " = '" + to + "' WHERE " + where + " = '" + id + "'"
+	stmt, _ := forum.DB.Prepare(update)
+	_, err := stmt.Exec()
+	if err != nil {
+		return err
+	}
+	// fmt.Print(stmt)
+	return nil
+}
+
+// Start of the Select query
