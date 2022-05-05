dd13432457a625c5832d73a8dd294e69fe9d7dab
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-05-05 01:04:39 +0100

    Auto format and check the length of the value of the session

--- frontend/js/myScript.js
+++ frontend/js/myScript.js
@@ -19,11 +19,11 @@ let fbtn_login = document.getElementById("form-btn-login");
 
 
 // When the user clicks the button, open the login modal 
-fbtn_login.onclick = function(event) {
+fbtn_login.onclick = function (event) {
   event.preventDefault();
   let data = new FormData();
 
- 
+
   data.append("userName", document.getElementById("userName").value);
   data.append("password", document.getElementById("password").value);
 
@@ -31,36 +31,37 @@ fbtn_login.onclick = function(event) {
   for (let [k, v] of data.entries()) { console.log(k, v); }
 
   fetch("http://localhost:8800/login",
-  {   method: 'POST',
-      body : data
-  })
-  .then(function(response) {
-    return response.text()
-  }).then(function(text) {
+    {
+      method: 'POST',
+      body: data
+    })
+    .then(function (response) {
+      return response.text()
+    }).then(function (text) {
       //text is the server's response
-      navbutdivnl.style.display ="none"
-      navbutdivl.style.display ="block"
+      navbutdivnl.style.display = "none"
+      navbutdivl.style.display = "block"
       lmodal.style.display = "none";
       console.log(text)
       lmodal.style.display = "none";
-  });
-  
+    });
+
 }
 
 // When the user clicks the button, open the login modal 
-lbtn.onclick = function() {
+lbtn.onclick = function () {
   lmodal.style.display = "block";
 }
 
 // When the user clicks on login , close the login modal and open the register modal
-rbtnl.onclick = function() {
+rbtnl.onclick = function () {
   rmodal.style.display = "block";
   lmodal.style.display = "none";
 }
 
 
 // When the user clicks on <span> (x), close the login modal
-lspan.onclick = function() {
+lspan.onclick = function () {
   lmodal.style.display = "none";
 }
 
@@ -87,7 +88,7 @@ let navbutdivl = document.getElementById("Login");
 
 
 // When the user clicks the button, open the login modal 
-fbtn_register.onclick = function(event) {
+fbtn_register.onclick = function (event) {
   event.preventDefault();
   let data = new FormData();
 
@@ -96,47 +97,48 @@ fbtn_register.onclick = function(event) {
   data.append("email", document.getElementById("rEmail").value);
 
   fetch("http://localhost:8800/register",
-  {   method: 'POST',
-      body : data
-  })
-  .then(function(response) {
-    return response.text()
-  }).then(function(text) {
+    {
+      method: 'POST',
+      body: data
+    })
+    .then(function (response) {
+      return response.text()
+    }).then(function (text) {
       //text is the server's response
       console.log(text)
-  });
+    });
 
-  
+
 
   for (let [k, v] of data.entries()) { console.log(k, v); }
-  navbutdivnl.style.display ="none"
-  navbutdivl.style.display ="block"
+  navbutdivnl.style.display = "none"
+  navbutdivl.style.display = "block"
   rmodal.style.display = "none";
 
 }
 
 
 // When the user clicks the button, open the login modal 
-rbtn.onclick = function() {
+rbtn.onclick = function () {
   rmodal.style.display = "block";
 }
 
 // When the user clicks on login , close the register modal and open the login modal
-lbtnr.onclick = function() {
+lbtnr.onclick = function () {
   rmodal.style.display = "none";
   lmodal.style.display = "block";
 }
 
 
 // When the user clicks on <span> (x), close the login modal
-rspan.onclick = function() {
+rspan.onclick = function () {
   rmodal.style.display = "none";
 }
 
 
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == lmodal) {
     lmodal.style.display = "none";
   } else if (event.target == rmodal) {
@@ -149,41 +151,39 @@ window.onclick = function(event) {
 
 // Get the button that logout the user
 let logoutBtn = document.getElementById("logoutBtn");
- 
+
 // When the user clicks on logoutBtn that logout the user
 
-logoutBtn.onclick = function() {
+logoutBtn.onclick = function () {
 
   let data = new FormData();
   console.log("text")
   fetch("http://localhost:8800/logout",
-  {   method: 'POST',
-      body : data
-  })
-  .then(function(response) {
-    return response.text()
-  }).then(function(text) {
+    {
+      method: 'POST',
+      body: data
+    })
+    .then(function (response) {
+      return response.text()
+    }).then(function (text) {
       //text is the server's response
       console.log(text)
-  });
+    });
 
   //data.append("Session", );
-  navbutdivnl.style.display ="flex"
-  navbutdivl.style.display ="none"
+  navbutdivnl.style.display = "flex"
+  navbutdivl.style.display = "none"
 }
-
-
 
 // onload
 
-const Onload = (cookie)=> {
+const Onload = (cookie) => {
 
   session = cookie.split("&")
-
-  if (session) {
-    navbutdivnl.style.display ="none"
-    navbutdivl.style.display ="block"
-  }
   console.log(session)
 
+  if (session.length != 1) {
+    navbutdivnl.style.display = "none"
+    navbutdivl.style.display = "block"
+  }
 }
